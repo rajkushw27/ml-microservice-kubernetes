465875692d324a8693ae87bf683ff89c4c6d440f
rajkushw27 <rajkushw27@gmail.com>
2020-04-11 23:05:14 +0530

    script for upload docker image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=rajkushw/aws-images
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag a3f7475893ea rajkushw/aws-images:predict_udacity
 
 # Step 3:
 # Push image to a docker repository
+docker push rajkushw/aws-images:predict_udacity
