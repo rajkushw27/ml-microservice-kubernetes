ffef94479851c13d9ec7e866d45664dfba156248
rajkushw27 <rajkushw27@gmail.com>
2020-04-12 00:08:54 +0530

    added script for deploying docker image in kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=dockerpath=rajkushw/aws-images
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run test-app --image=docker.io/rajkushw/aws-images:predict_udacity
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/predict_udacity 8000:80
+
 
